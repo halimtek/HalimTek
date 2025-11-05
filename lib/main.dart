77ba9a3306dfc19691e8bbfb3994b34154cd8ca4
HackingtheHack <abdihalim487@gmail.com>
2025-11-05 10:07:30 +0300

    remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
 void main() => runApp(MaterialApp(
   home: Home(),
 ));
@@ -53,58 +50,3 @@ class Home extends StatelessWidget {
   );
   }
 }
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Halim Teknoloji',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   // true => green, false => blue
-//   bool _isGreen = true;
-
-//   void _toggleColor() {
-//     setState(() {
-//       _isGreen = !_isGreen;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Halim Teknoloji'),
-//         actions: [
-//           IconButton(
-//             tooltip: 'Toggle background color',
-//             icon: const Icon(Icons.color_lens),
-//             onPressed: _toggleColor,
-//           ),
-//         ],
-//       ),
-//       // set the scaffold background color to green or blue
-//       backgroundColor: _isGreen ? Colors.green[100] : Colors.blue[100],
-//       body: Center(
-//         child: Text(
-//           _isGreen ? 'Background: Green' : 'Background: Blue',
-//           style: const TextStyle(fontSize: 18),
-//         ),
-//       ),
-//     );
-//   }
-// }
